5a2ce3484876166785a3db5b79f03a902e4c1512
pnn69 <pnn_69@hotmail.com>
2021-07-05 14:39:02 +0200

    First commit

---
+++ src/credentials.h
@@ -0,0 +1,20 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+//#define work
+    #ifdef work
+        const char* ssid = "airsupplies";
+        const char* password = "02351228133648477429";
+        const char* mqtt_server = "95.97.132.178";
+        const char* domoticz_server = "95.97.132.178";
+    #else
+        const char* ssid = "NicE_Engineering_UPC";
+        const char* password = "1001100110";
+        const char* mqtt_server = "192.168.1.11";
+        const char* domoticz_server = "192.168.1.11";
+    #endif
+    const char* domoticz_port = "1883";
+    const char* mqtt_user = "alibabba";
+    const char* mqtt_password = "sesamopenu";
+    const char *OTAhost = "OTAESP32";
+    
+#endif
